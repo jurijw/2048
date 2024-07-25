ca649655a3db739d496c3fe291eb36e5779653a4
Jurij Waite <jurijlwaite@gmail.com>
2024-07-25 21:19:52 +0900

    Added more tests. Ensured that inputs to destructive methods are deepcopied first.

--- test_board.py
+++ test_board.py
@@ -1,5 +1,6 @@
-import pytest
 from board import Board
+from copy import deepcopy
+import pytest
 
 collapsable_lists = [
     [2, 2, 2, 2],
@@ -8,6 +9,9 @@ collapsable_lists = [
     [0, 0, 2, 2],
     [2, 4, 4, 2],
     [2, 4, 4, 4],
+    [2, 0, 0, 2],
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
 ]
 
 collapsable_lists_expected = [
@@ -17,13 +21,16 @@ collapsable_lists_expected = [
     [4, 0, 0, 0],
     [2, 8, 2, 0],
     [2, 8, 4, 0],
+    [4, 0, 0, 0],
+    [0, 0, 0, 0],
+    [1, 0, 0, 0],
 ]
+
+def test_collapse_destructive():
+    for input, expected in zip(deepcopy(collapsable_lists), collapsable_lists_expected):
+        Board.collapse_destructive(input)
+        assert input == expected
 
 def test_collapse_non_destructive():
     for input, expected in zip(collapsable_lists, collapsable_lists_expected):
         assert Board.collapse_non_destructive(input) == expected
-
-def test_collapse_destructive():
-    for input, expected in zip(collapsable_lists, collapsable_lists_expected):
-        Board.collapse_destructive(input)
-        assert input == expected
