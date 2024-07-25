c4a71c4804cddafa97307fa302004690bb118e20
Jurij Waite <jurijlwaite@gmail.com>
2024-07-25 22:38:21 +0900

    Modified tests to test for gained points as well.

--- test_board.py
+++ test_board.py
@@ -26,11 +26,26 @@ collapsable_lists_expected = [
     [1, 0, 0, 0],
 ]
 
+points_expected = [
+    8,
+    0,
+    4,
+    4,
+    8,
+    8,
+    4,
+    0,
+    0,
+]
+
 def test_collapse_destructive():
-    for input, expected in zip(deepcopy(collapsable_lists), collapsable_lists_expected):
-        Board.collapse_destructive(input)
-        assert input == expected
+    for input_list, expected_list, expected_points in zip(deepcopy(collapsable_lists), collapsable_lists_expected, points_expected):
+        points = Board.collapse_destructive(input_list)
+        assert input_list == expected_list, f"Failed with input {input_list}."
+        assert points == expected_points, f"Failed with input {input_list}."
 
 def test_collapse_non_destructive():
-    for input, expected in zip(collapsable_lists, collapsable_lists_expected):
-        assert Board.collapse_non_destructive(input) == expected
+    for input_list, expected_list, expected_points in zip(collapsable_lists, collapsable_lists_expected, points_expected):
+        points, ouput_list = Board.collapse_non_destructive(input_list)
+        assert ouput_list == expected_list, f"Failed with input {input_list}."
+        assert points == expected_points, f"Failed with input {input_list}."
