ee714d097e2e635d9a008a8b3e1e950c5c0f7745
Jurij Waite <jurijlwaite@gmail.com>
2024-07-27 15:56:10 -0700

    Fixes to cli when incorrect input is entered.

--- cli.py
+++ cli.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from game import Game
 from moves import Moves
 from state import State
@@ -10,10 +11,11 @@ class CLI(Game):
 
     def get_move(self) -> Moves:
         """Parse user input until a valid move is entered."""
-        user_input = input("Enter a move (hjkl) ")
-        parsed_move = self.parse_move(user_input)
+        parsed_move = None
+        self.prompt_input()
         while parsed_move is None or parsed_move not in self.legal_moves:
-            parsed_move = self.parse_move(user_input)
+            user_input = input()
+            parsed_move = self.parse_move(user_input, self.do_on_incorrect_input)
         return parsed_move
 
     def do_before_game(self) -> None:
@@ -36,8 +38,13 @@ class CLI(Game):
         print("Game over ;-;")
 
     @staticmethod
-    def parse_move(user_input):
-        """Parses input from stdin into a move. Invalid input returns None."""
+    def prompt_input():
+        print("Enter a move (hjkl): ", end="")
+
+    @staticmethod
+    def parse_move(user_input: str, on_fail: Callable | None = None):
+        """Parses input from stdin into a move. Invalid input returns None and
+        calls the optional callback function ON_FAIL."""
         match user_input.strip().lower():
             case "h":
                 return Moves.LEFT
@@ -47,12 +54,20 @@ class CLI(Game):
                 return Moves.UP
             case "l":
                 return Moves.RIGHT
+        if on_fail is not None:
+            on_fail()
         return None
 
     @staticmethod
     def clear() -> None:
         """Runs the clear screen command for the current os."""
         os.system("clear" if os.name == "posix" else "cls")
+
+    def do_on_incorrect_input(self) -> None:
+        """Redraws the screen."""
+        self.clear()
+        self.do_before_every_move()
+        self.prompt_input()
 
     @staticmethod
     def add_padding(msg: str, left: int, above: int, below: int):
