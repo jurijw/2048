6196ec7fb7e092edf33947360ee7bf09141c240c
Jurij Waite <jurijlwaite@gmail.com>
2024-07-27 04:57:51 -0700

    Added a GridSlice class which is a view allowing interaction with arbitrary rows or columns of the grid as though they were regular lists.

--- grid.py
+++ grid.py
@@ -1,28 +1,39 @@
-from os import wait3
+from collections import namedtuple
+
+GridIndex = namedtuple("GridIndex", ["row", "col"])
 
 
 class Grid:
-    WIDTH: int = 4
-    HEIGHT: int = 4
-    SIZE: int = WIDTH * HEIGHT
+    DEFAULT_WIDTH: int = 4
+    DEFAULT_HEIGHT: int = 4
 
-    def __init__(self, width, height) -> None:
-        self._width = width
-        self._height = height
-        self._grid = [[0 for _ in range(width)] for _ in range(height)]
-
-    def apply_to_row(f, reverse=False):
-        pass
+    def __init__(self, grid=None) -> None:
+        if grid is None:
+            grid = [
+                [0 for _ in range(self.DEFAULT_WIDTH)]
+                for _ in range(self.DEFAULT_HEIGHT)
+            ]
+        self._grid = grid
+        self._width = len(grid[0])
+        self._height = len(grid)
 
     @property
     def grid(self):
         return self._grid
 
-    def get(self, row, col):
-        return self._grid[row][col]
+    @property
+    def width(self):
+        return self._width
 
-    def set_(self, row, col, val):
-        self._grid[row][col] = val
+    @property
+    def height(self):
+        return self._height
+
+    def __getitem__(self, idx: GridIndex):
+        return self._grid[idx.row][idx.col]
+
+    def __setitem__(self, idx: GridIndex, val: int):
+        self._grid[idx.row][idx.col] = val
 
     @property
     def max(self):
@@ -44,3 +55,24 @@ class Grid:
                 board_str += " " * padding + str(val) + " "
             board_str += "\n"
         return board_str
+
+
+class GridSlice:
+    def __init__(self, grid: Grid, select_rows: bool, axis: int) -> None:
+        self._grid = grid
+        self._select_rows = select_rows
+        self._axis = axis
+
+    def __getitem__(self, index: int):
+        if self._select_rows:
+            return self._grid[GridIndex(self._axis, index)]
+        return self._grid[GridIndex(index, self._axis)]
+
+    def __setitem__(self, index: int, val):
+        if self._select_rows:
+            self._grid[GridIndex(self._axis, index)] = val
+        else:
+            self._grid[GridIndex(index, self._axis)] = val
+
+    def __str__(self) -> str:
+        return "[" + ", ".join([str(val) for val in self]) + "]"
