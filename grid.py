dce0c435c08d33e005526c4f8c694403c942778b
Jurij Waite <jurijlwaite@gmail.com>
2024-07-27 05:49:33 -0700

    Changed Grid to implement a single list behind the scenes.

--- grid.py
+++ grid.py
@@ -7,15 +7,27 @@ class Grid:
     DEFAULT_WIDTH: int = 4
     DEFAULT_HEIGHT: int = 4
 
-    def __init__(self, grid=None) -> None:
+    def __init__(self, grid=None, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT) -> None:
         if grid is None:
-            grid = [
-                [0 for _ in range(self.DEFAULT_WIDTH)]
-                for _ in range(self.DEFAULT_HEIGHT)
-            ]
+            grid = [0 for _ in range(self.DEFAULT_WIDTH * self.DEFAULT_HEIGHT)]
         self._grid = grid
-        self._width = len(grid[0])
-        self._height = len(grid)
+        self._width = width
+        self._height = height
+
+    def __getitem__(self, idx: GridIndex):
+        return self._grid[self.linearized_index(idx)]
+
+    def __setitem__(self, idx: GridIndex, val: int):
+        self._grid[self.linearized_index(idx)] = val
+
+    def linearized_index(self, idx: GridIndex):
+        return self.width * idx.row + idx.col
+
+    def row(self, row_index):
+        return GridSlice(self, select_rows=True, axis=row_index)
+
+    def col(self, col_index):
+        return GridSlice(self, select_rows=False, axis=col_index)
 
     @property
     def grid(self):
@@ -29,32 +41,23 @@ class Grid:
     def height(self):
         return self._height
 
-    def __getitem__(self, idx: GridIndex):
-        return self._grid[idx.row][idx.col]
-
-    def __setitem__(self, idx: GridIndex, val: int):
-        self._grid[idx.row][idx.col] = val
-
     @property
     def max(self):
         """Return the largest entry in the grid."""
-        max = 0
-        for row in self.grid:
-            for val in row:
-                if val > max:
-                    max = val
-        return max
+        return max(self._grid)
 
     def __str__(self) -> str:
         """Return a readable string representation of the baord, in which all values are displayed in a column aligned format."""
         max_digits = len(str(self.max))
         board_str: str = ""
-        for row in self.grid:
-            for val in row:
+        for row in range(self.height):
+            row_str = ""
+            for col in range(self.width):
+                val = self[GridIndex(row, col)]
                 padding = max_digits - len(str(val))
-                board_str += " " * padding + str(val) + " "
-            board_str += "\n"
-        return board_str
+                row_str += " " * padding + str(val) + " "
+            board_str += row_str.strip() + "\n"
+        return board_str.strip()
 
 
 class GridSlice:
@@ -76,3 +79,6 @@ class GridSlice:
 
     def __str__(self) -> str:
         return "[" + ", ".join([str(val) for val in self]) + "]"
+
+    def __repr__(self) -> str:
+        return f"{__class__.__name__} select_rows={self._select_rows} axis={self._axis}"
